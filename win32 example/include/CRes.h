0087c0c4592d93adc0520245d66e582c8a30a973
orange2458 <asm1133@naver.com>
2023-12-05 23:18:43 +0900

    Resource(1)

---
+++ win32 example/include/CRes.h
@@ -0,0 +1,18 @@
+#pragma once
+
+class CRes
+{
+public:
+	CRes( );
+	~CRes( );
+
+	void SetKey( const std::wstring& _strKey ) { m_strKey = _strKey; }
+	void SetRelativePath( const std::wstring& _strPath ) { m_strRelativePath = _strPath; }
+
+	const std::wstring& GetKey( ) { return m_strKey; }
+	const std::wstring& GetRelativePath( ) { return m_strRelativePath; }
+
+private:
+	std::wstring m_strKey;				// 리소스 키값
+	std::wstring m_strRelativePath;		// 리소스 상대경로
+};
